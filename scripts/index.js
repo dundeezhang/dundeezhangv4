518c337a0c6a3d033965414f53be2f1086f14c85
Dundee <dundee_zhang@hotmail.com>
2023-01-06 21:47:06 -0500

    cover page added

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+let height = screen.height;
+
+$(".cover-page").click(function() {
+    $(".profile-picture, .cover-page, .see-more").animate({
+        opacity: 0.7
+    }, 100);
+
+    $('html, body').animate({
+        scrollTop: $(".content").offset().top
+    }, 100);
+    
+});
