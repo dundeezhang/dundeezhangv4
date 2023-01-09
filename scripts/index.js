275f3af4d716ef4001b55af92049bc8eec78d3fd
Dundee <dundee_zhang@hotmail.com>
2023-01-09 17:34:03 -0500

    add about me content

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,32 @@
-let height = screen.height;
+let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
-$(".cover-page").click(function() {
-    $(".profile-picture, .cover-page, .see-more").animate({
+let vmax = Math.max(h, w);
+let pictureNewSize = vmax * 0.20;
+let nameNewSize = vmax * 2.5;
+$(".cover-page").click(function () {
+    $(".profile-picture, .cover-page").animate({
         opacity: 0.7
     }, 100);
 
+    $(".see-more").animate({
+        opacity: 0
+    }, 100);
+
+    $(".profile-picture").animate({
+        width: pictureNewSize
+    }, 100);
+
+    $(".name").animate({
+        fontsize: nameNewSize
+    }, 100);
+
     $('html, body').animate({
-        scrollTop: $(".content").offset().top
+        scrollTop: $(".introduction").offset().top
     }, 100);
 
     $("html, body").css({
-        "overflow":"visible"
+        "overflow": "visible"
     });
 });
 
