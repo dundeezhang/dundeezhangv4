ad755301980b206a54f6f757becd48d550c5dece
Dundee <dundee_zhang@hotmail.com>
2024-07-04 19:36:02 -0400

    remove dumb animations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import pfp from './img/cropped_image.png';
-import './css/App.css';
+import pfp from "./img/cropped_image.png";
+import "./css/App.css";
 
 function App() {
     return (
@@ -8,32 +8,80 @@ function App() {
                 <img src={pfp} className="App-logo" alt="profile" />
                 <h1 className="ibm-plex-mono-regular">I'm Dundee</h1>
                 <p>
-                    a <a target="_blank" rel="noreferrer" className="csText" href="https://uwaterloo.ca/future-students/programs/computer-science">Computer Science</a> student at the <a target="_blank" rel="noreferrer" className="schoolText" href="https://uwaterloo.ca">University of Waterloo</a>
+                    a{" "}
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                        className="csText"
+                        href="https://uwaterloo.ca/future-students/programs/computer-science"
+                    >
+                        Computer Science
+                    </a>{" "}
+                    student at the{" "}
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                        className="schoolText"
+                        href="https://uwaterloo.ca"
+                    >
+                        University of Waterloo
+                    </a>
                 </p>
                 <p>
-                    I am a developer who is passionate about pushing technology forward!
+                    I am a developer who is passionate about pushing technology
+                    forward!
                 </p>
-                
+
                 <ul className="social-cons">
                     <li>
-                        <a rel="noreferrer" href="https://www.linkedin.com/in/dundeezhang/" style={{color: "black", fontSize: "calc(10px + 2vmin)"}} target="_blank">
+                        <a
+                            rel="noreferrer"
+                            href="https://www.linkedin.com/in/dundeezhang/"
+                            style={{
+                                color: "black",
+                                fontSize: "calc(10px + 2vmin)",
+                            }}
+                            target="_blank"
+                        >
                             <i class="fa-brands fa-linkedin-in"></i>
                         </a>
-                        
                     </li>
                     <li>
-                        <a rel="noreferrer" href="https://github.com/dundeezhang" style={{color: "black", fontSize: "calc(10px + 2vmin)"}} target="_blank">
-                        <i class="fa-brands fa-github"></i>
+                        <a
+                            rel="noreferrer"
+                            href="https://github.com/dundeezhang"
+                            style={{
+                                color: "black",
+                                fontSize: "calc(10px + 2vmin)",
+                            }}
+                            target="_blank"
+                        >
+                            <i class="fa-brands fa-github"></i>
                         </a>
                     </li>
                     <li>
-                        <a rel="noreferrer" href="mailto:dundeez124@gmail.com" style={{color: "black", fontSize: "calc(10px + 2vmin)"}} target="_blank">
+                        <a
+                            rel="noreferrer"
+                            href="mailto:dundeez124@gmail.com"
+                            style={{
+                                color: "black",
+                                fontSize: "calc(10px + 2vmin)",
+                            }}
+                            target="_blank"
+                        >
                             <i class="fa-regular fa-envelope"></i>
                         </a>
                     </li>
                 </ul>
-                <p  className="ibm-plex-mono-regular resume resume-hover">
-                    <a className="resume-hover" href="https://drive.google.com/file/d/1bEVO4IF-KCmf9nbOUBesm1kiag_9tWOq/view?usp=sharing" target="_blank" rel="noreferrer">--{'>'} Resume {'<'}--</a>
+                <p className="ibm-plex-mono-regular resume resume-hover">
+                    <a
+                        className="resume-hover"
+                        href="https://drive.google.com/file/d/1bEVO4IF-KCmf9nbOUBesm1kiag_9tWOq/view?usp=sharing"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        --{">"} Resume {"<"}--
+                    </a>
                 </p>
             </header>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './css/index.css';
-import App from './App';
-import Works from './Works';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./css/index.css";
+import App from "./App";
+import Works from "./Works";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-    <Works />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+        <Works />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
